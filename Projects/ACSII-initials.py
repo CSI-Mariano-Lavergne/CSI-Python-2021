d34a75c9d984b05087926b1017b91744500295ef
CSI-Mariano-Lavergne <24-045@sanignacio.pr>
2021-09-01 14:47:12 -0400

    Initials

---
+++ Projects/ACSII-initials.py
@@ -0,0 +1,9 @@
+print('''
+m     m  n    n  l      v    v
+mm   mm  nn   n  l      v    v
+mm   mm  n  n n  l      v    v
+m  m  m  n   nn  l      v    v
+m     m  n    n  l      v    v
+m     m  n    n  l       v  v
+m     m  n    n  lllll    v    
+''')
