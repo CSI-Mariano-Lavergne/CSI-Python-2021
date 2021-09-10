85f0f7c6492b5c02bd6c47f3a476d82b7b5f8755
CSI-Mariano-Lavergne <24-045@sanignacio.pr>
2021-09-10 14:25:00 -0400

    Script Food

---
+++ Modules/Module4/Module4.py
@@ -0,0 +1,9 @@
+print("What did you have for Breakfast?")
+meal = 'egg sandwich'
+print(meal)
+print("What was your Lunch?")
+meal = "burrito with nachos"
+print(meal)
+print("Have you had dinner?")
+meal = "No not yet"
+print(meal)
