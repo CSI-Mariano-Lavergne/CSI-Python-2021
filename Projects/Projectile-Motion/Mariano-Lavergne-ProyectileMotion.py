ed13b7a29b5e213fc69188ae241f662fd8d674ba
CSI-Mariano-Lavergne <24-045@sanignacio.pr>
2021-10-28 14:36:15 -0400

    projectile

--- Projects/Projectile-Motion/Mariano-Lavergne-ProyectileMotion.py
+++ Projects/Projectile-Motion/Mariano-Lavergne-ProyectileMotion.py
@@ -1,17 +1,18 @@
-gun = "FN GL40"
-cartridge = "40X46 mm"
-ammunition_type = "Single"
-projectile_velocity = 30
-building = "Caribbean Sea View"
-building_height = 334
-gravity = 9.81
-
 import math
 
-time_s = math.sqrt((2 * building_height) / gravity)
+# gun = "FN GL40"
+# cartridge = "40X46 mm"
+# ammunition_type = "M381 grenade"
+# projectile_velocity = 76
+# building = "Caribbean Sea View"
+# building_height = 334
+# gravity = 9.81
 
-distance = (projectile_velocity * time_s)
+def ProjectileFunction(gun:str, cartridge:str, ammunition_type:str, projectile_velocity:int, building:str, building_height:int, gravity:int):
+    time_s = math.sqrt((2 * building_height) / gravity)
+    distance = (projectile_velocity * time_s)
 
-print(f"The gun selected was {gun}")
+    print(f"The gun i chose was a grenade launcher. The offcial gun name is {gun}. Its cartridge size is {cartridge}. The type of bullet that it takes is a {ammunition_type}. Lastly it shoots a grenade at a speed at {projectile_velocity} mi/s. ")
 
-print("The gun i chose was a grenade launcher. The offcial gun name is the FN GL40. Its cartridge size is 40x46 mm. It takes single ammunition meaning that it can only shoot one grenade at a time. Lastly it shoots a grenade at a speed at 30mi/s. ")
+
+ProjectileFunction("FN GL40", "40X46mm", "M381 grenade", 76, "Caribbean Sea View", 334, 9.81)
