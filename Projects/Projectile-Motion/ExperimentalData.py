2c1a853b7bb1e43a58bef1783ba9db3cb1776362
CSI-Mariano-Lavergne <24-045@sanignacio.pr>
2021-11-03 14:56:20 -0400

    Experimental Data

---
+++ Projects/Projectile-Motion/ExperimentalData.py
@@ -0,0 +1,9 @@
+class ExperimentalData:
+    def __init__(self, gun:str, cartridge:str, ammunition_type:str, projectile_velocity:int, building:str, building_height:int, gravity:int):
+        self.gun = gun
+        self.cartridge = cartridge
+        self.ammunition_type = ammunition_type
+        self.projectile_velocity = projectile_velocity
+        self.building = building
+        self.building_height = building_height
+        self.gravity = gravity
--- Projects/Projectile-Motion/Mariano-Lavergne-ProyectileMotion.py
+++ Projects/Projectile-Motion/Mariano-Lavergne-ProyectileMotion.py
@@ -1,5 +1,5 @@
 import math
-
+from ExperimentalData import ExperimentalData
 # gun = "FN GL40"
 # cartridge = "40X46 mm"
 # ammunition_type = "M381 grenade"
@@ -8,11 +8,25 @@ import math
 # building_height = 334
 # gravity = 9.81
 
-def ProjectileFunction(gun:str, cartridge:str, ammunition_type:str, projectile_velocity:int, building:str, building_height:int, gravity:int):
+def ProjectileFunction(experimentalData:ExperimentalData):
+
     time_s = math.sqrt((2 * building_height) / gravity)
+    # distance = (experimentalData[projectile_velocity] * time_s)
     distance = (projectile_velocity * time_s)
 
     print(f"The gun i chose was a grenade launcher. The offcial gun name is {gun}. Its cartridge size is {cartridge}. The type of bullet that it takes is a {ammunition_type}. Lastly it shoots a grenade at a speed at {projectile_velocity} mi/s. ")
 
 
 ProjectileFunction("FN GL40", "40X46mm", "M381 grenade", 76, "Caribbean Sea View", 334, 9.81)
+
+# experimentalData = {
+# "gun" : "FN GL40", 
+# "cartridge" : "40X46 mm", 
+# "ammunition_type" : "M381 grenade",
+# "projectile_velocity" : 76,
+# "building" : "Caribbean Sea View",
+# "building_height" : 334,
+# "gravity" : 9.81
+# }
+
+experimentalData = ExperimentalData("FN GL40", "40X46mm", "M381 grenade", 76, "Caribbean Sea View", 334, 9.8)
