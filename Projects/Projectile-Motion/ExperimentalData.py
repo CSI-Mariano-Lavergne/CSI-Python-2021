5f9a88db7a2c9c06e423482ff010f753504b2bef
CSI-Mariano-Lavergne <24-045@sanignacio.pr>
2021-11-11 15:03:32 -0400

    Projectile Motion

--- Projects/Projectile-Motion/ExperimentalData.py
+++ Projects/Projectile-Motion/ExperimentalData.py
@@ -1,9 +1,10 @@
 class ExperimentalData:
-    def __init__(self, gun:str, cartridge:str, ammunition_type:str, projectile_velocity:int, building:str, building_height:int, gravity:int):
+    def __init__(self, gun:str, cartridge:str, ammunition_type:str, projectile_velocity:int, building:str, building_height:int, planets:str):
         self.gun = gun
         self.cartridge = cartridge
         self.ammunition_type = ammunition_type
         self.projectile_velocity = projectile_velocity
         self.building = building
         self.building_height = building_height
-        self.gravity = gravity
+        self.planets = planets
+        
--- Projects/Projectile-Motion/Mariano-Lavergne-ProyectileMotion.py
+++ Projects/Projectile-Motion/Mariano-Lavergne-ProyectileMotion.py
@@ -13,11 +13,22 @@ import os
 
 def ProjectileFunction(experimentalData:ExperimentalData):
 
-    time_s = math.sqrt((2 * experimentalData.building_height) / experimentalData.gravity)
+    # Parallel Lists
+    planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+    g_ms2 = [3.7, 8.87, 9.81, 3.711, 24.79, 10.44, 8.69, 11.15]
+
+# Planet Index
+    planet_index = planets.index(input("Planet Name: "))
+
+# Planet Gravity
+    g_ms2[planet]
+
+
+    time_s = math.sqrt((2 * experimentalData.building_height) / experimentalData.planets
     # distance = (experimentalData[projectile_velocity] * time_s)
     distance = (experimentalData.projectile_velocity * time_s)
 
-    print(f"The gun i chose was a grenade launcher. The offcial gun name is {experimentalData.gun}. Its cartridge size is {experimentalData.cartridge}. The type of bullet that it takes is a {experimentalData.ammunition_type}. Lastly it shoots a grenade at a speed at {experimentalData.projectile_velocity} mi/s. The time the bullet took to travel {distance} meters was {time_s} seconds ")
+    print(f"The gun i chose was a grenade launcher. The offcial gun name is {experimentalData.gun}. Its cartridge size is {experimentalData.cartridge}. The type of bullet that it takes is a {experimentalData.ammunition_type}. Lastly it shoots a grenade at a speed at {experimentalData.projectile_velocity} mi/s. The time the bullet took to travel to the bottom of the {experimentalData.building} which is a distance{distance} meters was {time_s} seconds ")
 
 
 # ProjectileFunction("FN GL40", "40X46mm", "M381 grenade", 76, "Caribbean Sea View", 334, 9.81)
@@ -32,13 +43,14 @@ def ProjectileFunction(experimentalData:ExperimentalData):
 # "gravity" : 9.81
 # }
 
-experimentalData = ExperimentalData("FN GL40", "40X46mm", "M381 grenade", 76, "Caribbean Sea View", 334, 9.8)
+# experimentalData = ExperimentalData("FN GL40", "40X46mm", "M381 grenade", 76, "Caribbean Sea View", 334, 9.8)
 
 myDataSet = [
 ExperimentalData("FN GL40", "40X46mm", "M381 grenade", 76, "Caribbean Sea View", 334, 9.8),
 ExperimentalData("FN GL40", "40X46mm", "M381 grenade", 76, "Burj Khalifa", 828, 9.8),
 ExperimentalData("FN GL40", "40X46mm", "M381 grenade", 76, "Shanghai Tower", 632, 9.8),   
-ExperimentalData("FN GL40", "40X46mm", "M381 grenade", 76, "Lotte World Tower", 555, 9.8) 
+ExperimentalData("FN GL40", "40X46mm", "M381 grenade", 76, "Lotte World Tower", 555, 9.8),
+ExperimentalData("FN GL40", "40X46mm", "M381 grenade", 76, "One World Trade Center", 541.3, 9.8) 
 ]
 
 for data in myDataSet:
@@ -50,3 +62,11 @@ for data in myDataSet:
 
     with open(myOutputFilePath, 'w') as outfile:
         json.dump([data.__dict__ for data in myDataSet], outfile)
+
+    
+deserialize = open(myOutputPath)
+experimentJson = json.load(deserialize)
+
+
+for e in experimentJson:
+    ExperimentalData(**e).run()
