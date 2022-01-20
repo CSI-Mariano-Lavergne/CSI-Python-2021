777f8fa6a63691f866369591697c9cedcefe6d9e
CSI-Mariano-Lavergne <24-045@sanignacio.pr>
2022-01-20 14:48:20 -0400

    Hangman

---
+++ Projects/Mariano-Lavergne-Hangman.py
@@ -0,0 +1,16 @@
+import random
+word_list = ["cagueños", "doradeños", "fajardeños", "humacaeños", "mayagüezanos", "sanjuaneros", "pepinianos", "ponceños", "rincoeños", "guaynabeños"]
+
+def display_hangman(tries):
+    stages = [""
+                    --------
+                    |    |
+                    |    o
+                    |   /|\\
+                    |    |
+                    |   / \\
+                    -
+    
+    
+    
+    ]
