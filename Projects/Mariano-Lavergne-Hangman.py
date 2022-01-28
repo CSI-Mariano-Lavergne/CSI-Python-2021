650668106f91327532c2305252613869ff39e33a
CSI-Mariano-Lavergne <24-045@sanignacio.pr>
2022-01-28 14:56:31 -0400

    Hangman

--- Projects/Mariano-Lavergne-Hangman.py
+++ Projects/Mariano-Lavergne-Hangman.py
@@ -1,5 +1,5 @@
 import random
-word_list = ["cagueños", "doradeños", "fajardeños", "humacaeños", "mayagüezanos", "sanjuaneros", "pepinianos", "ponceños", "rincoeños", "guaynabeños"]
+word_list = ["caguenos", "doradenos", "fajardenos", "humacaenos", "mayaguezanos", "sanjuaneros", "pepinianos", "poncenos", "rincoenos", "guaynabenos"]
 
 def get_word(word_list):
     word = random.choice(word_list)
@@ -27,6 +27,34 @@ def play(word):
                 guessed_letters.append(guess)
             else:
                 print("Nice one", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already tried", guess, "!")
+            elif guess != word:
+                print(guess, "Is not the word")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else:
+            print("Ivalid input")
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
+
+    if guessed:
+        print("Good job, you guessed the word")
+    else:
+        print("You ran of tries. The word was" + word + "Maybe next time")
                 
 def display_hangman(tries):
     stages = [   """
@@ -93,3 +121,14 @@ def display_hangman(tries):
                         -
                         """,
     ]
+    return stages[tries]
+def main():
+    word = get_word(word_list)
+    play (word)
+    while input("Again?  (Y/N)").upper() == "Y":
+        wprd = get_word(word_list)
+        play(word)
+
+
+if __name__ == "__main__":
+    main()
